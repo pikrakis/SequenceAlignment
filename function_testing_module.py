9b0213c13d6f7d4a280828c84f170cf051cad951
Aggelos Pikrakis <aggelos.pikrakis@gmail.com>
2017-12-04 09:07:41 +0200

    testing module updated with plotting capabilities

--- function_testing_module.py
+++ function_testing_module.py
@@ -2,6 +2,8 @@
 
 import sequence_alignment
 import other_functions
+import matplotlib.pyplot as plt
+import numpy as np
 
 test = [-1, 2, 1, -1, -1, 2]
 ref = [-1, 2, 1, -1, 2]
@@ -9,3 +11,10 @@ c, s, predi, predj = sequence_alignment.dtw_distance_ita(test,ref)
 print('\ndtw cost = ' + str(c) + '\n')
 bp = other_functions.extract_best_path(s, predi, predj)
 print('best path = ' + str(bp))
+bp = np.array(bp)
+plt.plot(bp[:, 1], bp[:, 0], 'ro')
+plt.plot(bp[:, 1], bp[:, 0], linewidth=2.0)
+plt.ylabel('test sequence')
+plt.xlabel('reference sequence')
+plt.title('alignment path')
+plt.show()
